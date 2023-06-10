abe560bfb5ded21e0d5973634ce91ce0b5b21813
uchario <uchario@yahoo.com>
2023-06-10 18:09:05 +0100

    add redux_demo

---
+++ redux_demo/redux-demo.js
@@ -0,0 +1,28 @@
+const redux = require('redux');
+
+const counterReducer = (state = {counter: 0}, action) => {
+    switch(action.type) {
+        case 'increment':
+            return {
+                counter: state.counter + 1
+            }
+        case 'decrement':
+            return {
+                counter: state.counter - 1
+            }
+        default:
+            return state;
+    }
+   
+}
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type: 'decrement'});
