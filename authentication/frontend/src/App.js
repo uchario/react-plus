740044f57b054cff5b6f35002d01406b44d0044f
uchario <uchario@yahoo.com>
2023-06-24 09:42:04 +0100

    Adding User Logout

--- authentication/frontend/src/App.js
+++ authentication/frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import Authentication, { action as authAction } from './pages/Authentication';
+import { action as logoutAction } from './pages/Logout';
 
 const router = createBrowserRouter([
   {
@@ -64,6 +65,10 @@ const router = createBrowserRouter([
         path: 'auth',
         element: <Authentication/>,
         action: authAction
+      },
+      {
+        path: 'logout',
+        action: logoutAction
       }
     ],
   },
