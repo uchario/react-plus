f3aee3d68bea6c3f941b8f36dbd9099d4bfebe6d
uchario <uchario@yahoo.com>
2023-06-25 00:36:04 +0100

    Testing asynchronous code

--- react_testing/src/App.js
+++ react_testing/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Greeting from './components/Greeting';
+import Async from './components/Async';
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <Greeting/>
+      <Async/>
     </div>
   );
 }
