2f453c9852d3c7cdb814504e282e3c932761cf08
uchario <uchario@yahoo.com>
2023-06-24 15:58:20 +0100

    Animation timing

--- animating_react_apps/src/components/Modal/Modal.js
+++ animating_react_apps/src/components/Modal/Modal.js
@@ -3,12 +3,17 @@ import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
+const animationTiming = {
+    enter: 1000,
+    exit: 2000
+}
+
 const modal = (props) => {
     
     return(
         <Transition
             in={props.show}
-            timeout={300}
+            timeout={animationTiming}
             mountOnEnter
             unmountOnExit
         >
