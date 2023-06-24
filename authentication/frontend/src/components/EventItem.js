e997dfd5a16e61585dfdb982a421320be8b967df
uchario <uchario@yahoo.com>
2023-06-24 09:59:21 +0100

    Updating the UI based on Auth status

--- authentication/frontend/src/components/EventItem.js
+++ authentication/frontend/src/components/EventItem.js
@@ -1,9 +1,10 @@
-import { Link, useSubmit } from 'react-router-dom';
+import { Link, useSubmit, useRouteLoaderData } from 'react-router-dom';
 
 import classes from './EventItem.module.css';
 
 function EventItem({ event }) {
   const submit = useSubmit();
+  const token = useRouteLoaderData('root');
 
   function startDeleteHandler() {
     const proceed = window.confirm('Are you sure?');
@@ -20,8 +21,20 @@ function EventItem({ event }) {
       <time>{event.date}</time>
       <p>{event.description}</p>
       <menu className={classes.actions}>
-        <Link to="edit">Edit</Link>
-        <button onClick={startDeleteHandler}>Delete</button>
+        {token && 
+          <Link 
+            to="edit"
+          >
+            Edit
+          </Link>
+        }
+        {token && 
+          <button 
+            onClick={startDeleteHandler}
+          >
+            Delete
+          </button>
+        }
       </menu>
     </article>
   );
